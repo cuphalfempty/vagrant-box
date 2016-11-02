2b17c42e0a48b605dac7cad7e4bd46f86f7455ed
Alek Wyka <aks@cuphalfempty.net>
2016-11-02 22:04:38 +0000

    Use ubuntu xenial server

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,6 +1,18 @@
 exec { "apt-get update":
   path => "/usr/bin",
 }
+package { "postgresql-9.5":
+  ensure => present,
+  require => Exec["apt-get update"],
+}
+package { "postgresql-client-9.5":
+  ensure => present,
+  require => Exec["apt-get update"],
+}
+service { "postgresql":
+  ensure => running,
+  require => Package["postgresql-9.5"],
+}
 package { "apache2":
   ensure => present,
   require => Exec["apt-get update"],
