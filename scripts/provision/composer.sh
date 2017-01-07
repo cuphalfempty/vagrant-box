a2e33e58cb432b58a635fbaba12035431d9d4036
Alek Wyka <aks@cuphalfempty.net>
2017-01-07 09:14:57 +0000

    Fix composer install as docs recommends

--- scripts/provision/composer.sh
+++ scripts/provision/composer.sh
@@ -1,7 +1,21 @@
-#!/bin/bash
+#!/bin/sh
 
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');" \
-	&& php -r "if (hash_file('SHA384', 'composer-setup.php') === '61069fe8c6436a4468d0371454cf38a812e451a14ab1691543f25a9627b97ff96d8753d92a00654c21e2212a5ae1ff36') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;" \
-	&& php composer-setup.php \
-	&& php -r "unlink('composer-setup.php');" \
-	&& sudo mv composer.phar /usr/local/bin/composer
+EXPECTED_SIGNATURE=$(wget -q -O - https://composer.github.io/installer.sig)
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+ACTUAL_SIGNATURE=$(php -r "echo hash_file('SHA384', 'composer-setup.php');")
+
+if [ "$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE" ]
+then
+    >&2 echo 'ERROR: Invalid installer signature'
+    rm composer-setup.php
+    exit 1
+fi
+
+php composer-setup.php --quiet
+RESULT=$?
+if [ $RESULT ]
+then
+	sudo mv composer.phar /usr/local/bin/composer
+fi
+rm composer-setup.php
+exit $RESULT
